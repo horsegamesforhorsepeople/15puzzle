d61f51d5c7fb75ebe8a0ea4e7ccb83068224d5e5
horsegamesforhorsepeople <134537165+horsegamesforhorsepeople@users.noreply.github.com>
2023-08-16 17:32:57 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,85 @@
+const pieceMap = new Map();
+
+function updateBoard() {
+  let hasWon = true;
+  for (let pieceIndex = 0; pieceIndex < 16; pieceIndex++) {
+    const currentPiece = document.getElementsByClassName("square")[pieceIndex];
+    currentPiece.dataset.value = pieceMap.get(pieceIndex);
+    currentPiece.textContent = pieceMap.get(pieceIndex);
+    if (pieceMap.get(pieceIndex) !== pieceIndex + 1 && pieceMap.get(pieceIndex) !== " ") hasWon = false;
+    currentPiece.dataset.status = pieceMap.get(pieceIndex) === pieceIndex + 1 ? "correct" : "wrong";
+  }
+  if (!hasWon) return;
+  
+  let button = document.createElement("button")
+  button.textContent = "play again";
+  document.body.append(button)
+  document.querySelector("button").onclick = function() {
+    initializeMap();
+  }
+}
+
+function initializeMap() {
+  if (document.querySelector("button")) document.querySelector("button").remove()
+  let availableNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, " "];
+  for (let i = 0; i < 16; i++) {
+    const randomIndex = Math.floor(Math.random() * availableNumbers.length);
+    const randomNumber = availableNumbers[randomIndex];
+    pieceMap.set(i, randomNumber);
+    availableNumbers.splice(randomIndex, 1);
+    if (document.getElementById("board").children.length !== 16) {
+      const square = document.createElement("div");
+      square.className = "square";
+      document.getElementById("board").append(square);
+    }
+  }
+  updateBoard();
+}
+
+initializeMap();
+
+document.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "ArrowDown": swapPieces(-4); break;
+    case "ArrowRight": swapPieces(-1); break;
+    case "ArrowLeft": swapPieces(1); break;
+    case "ArrowUp": swapPieces(4); break;
+  }
+})
+
+function swapPieces(offset) {
+  const blankIndex = getKey(" ");
+
+  // return if there is no valid piece at the offset position
+  if ((blankIndex + 1) % 4 === 0 && offset === 1) return; // right
+  else if (blankIndex > 11 && offset === 4) return;       // bottom
+  else if (blankIndex % 4 === 0 && offset === -1) return; // left
+  else if (blankIndex < 4 && offset === -4) return;       // up
+
+  const movingIndex = blankIndex + offset;
+  pieceMap.set(blankIndex, pieceMap.get(movingIndex));
+  pieceMap.set(movingIndex, " ");
+  /*
+  pieceMap.set(0, 1)
+  pieceMap.set(1, 2)
+  pieceMap.set(2, 3)
+  pieceMap.set(3, 4)
+  pieceMap.set(4, 5)
+  pieceMap.set(5, 6)
+  pieceMap.set(6, 7)
+  pieceMap.set(7, 8)
+  pieceMap.set(8, 9)
+  pieceMap.set(9, 10)
+  pieceMap.set(10, 11)
+  pieceMap.set(11, 12)
+  pieceMap.set(12, 13)
+  pieceMap.set(13, 14)
+  pieceMap.set(14, 15)
+  pieceMap.set(15, " ")
+  */
+  updateBoard()
+
+  function getKey(val) {
+    return [...pieceMap].find(([key, value]) => val === value)[0];
+  }
+}
